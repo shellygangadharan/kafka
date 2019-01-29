73d1b2b1a4614402d6825090b7ac509e0a08b898
shellygangadharan <shelly.gangadharan@gmail.com>
2019-01-28 22:57:53 -0500

    changes for producer

---
+++ kafkaproducer/src/main/java/com/git/shelly/kafka/tutorial/ProducerDemo.java
@@ -0,0 +1,24 @@
+package com.git.shelly.kafka.tutorial;
+
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+public class ProducerDemo {
+
+    public static void main(String[] args){
+        System.out.println(" hello world!") ;
+        final String BOOT_STRAP_SERVER="127.0.0.1:9092";
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,BOOT_STRAP_SERVER) ;
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String,String>(properties);
+        ProducerRecord<String,String>  record = new ProducerRecord<String,String> ("new_topic2","hello world");
+        producer.send(record) ;
+        producer.flush();
+        producer.close();
+    }
+}
